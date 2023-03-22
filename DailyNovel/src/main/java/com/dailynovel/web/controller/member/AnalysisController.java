f095d7eb680fc7c10fdf1b954b9d308eb3a3fb3e
joobro <joobro2015@gmail.com>
2023-03-22 12:04:51 +0900

    design(analysis):css 수정

--- DailyNovel/src/main/java/com/dailynovel/web/controller/member/AnalysisController.java
+++ DailyNovel/src/main/java/com/dailynovel/web/controller/member/AnalysisController.java
@@ -1,27 +1,67 @@
 package com.dailynovel.web.controller.member;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dailynovel.web.entity.FeelingList;
+import com.dailynovel.web.service.FeelingService;
 
 @Controller
 @RequestMapping("/analysis")
 public class  AnalysisController {
 
+	@Autowired
+	private FeelingService service;
+	
+	
+//	@GetMapping("feeling")
+//	public String feeling(Model model) {
+//		List<Feeling> list = service.getList();
+//		model.addAttribute("list", list);
+//		return "analysis/feeling";
+//	}
+	
+//	@GetMapping("feeling-chart")
+//	public String feeling-chart(Model model) {
+//		List<Feeling> list = service.getList();
+//		model.addAttribute("list", list);
+//		return "analysis/feeling-chart";
+//	}
+//	
+	@GetMapping("feeling-view-detail")
+	public String feelingViewDetail(Model model) {
+		List<FeelingList> list = service.getList();
+		
+		model.addAttribute("list", list);
+		return "analysis/looking-back/feeling-view-details";
+	}
+//	
+//	@GetMapping("honesty")
+//	public String honesty(Model model) {
+//		List<Feeling> list = service.getList();
+//		model.addAttribute("list", list);
+//		return "analysis/honesty";
+//	}
+//	
+//	@GetMapping("How")
+//	public String How(Model model) {
+//		List<Feeling> list = service.getList();
+//		model.addAttribute("list", list);
+//		return "analysis/How";
+//	}
+//	
+//	@GetMapping("main")
+//	public String main(Model model) {
+//		List<Feeling> list = service.getList();
+//		model.addAttribute("list", list);
+//		return "analysis/main";
+//	}
+	
 
-	@RequestMapping("/main")
-	public String main(){
-		return "/WEB-INF/view/member/diary/analysis/main.jsp";
-	}
-	
-	@RequestMapping("/feeling")
-	public String setting(){
-		return "/WEB-INF/view/member/diary/analysis/component/feeling.jsp";
-	}
-	
-	@RequestMapping("/honesty")
-	public String honesty(){
-		return "/WEB-INF/view/member/diary/analysis/component/honesty.jsp";
-	}
-	
 
 }
